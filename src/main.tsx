7d9bdf48a67ce307d9fd818bbf5de035aec8b204
divinirakiza <irakizadivin@gmail.com>
2022-05-04 11:14:14 +0200

    refactor!: upgrade to react router v6

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import {BrowserRouter as Router} from 'react-router-dom'
-import './styles/index.scss'
+import './styles/index.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-  </React.StrictMode>
-)
+    <App />
+  </React.StrictMode>,
+);
--- src/pages/auth/Signin.tsx
+++ src/pages/auth/Signin.tsx
@@ -1,36 +1,40 @@
 import '../../styles/pages/Signin.scss';
+
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+
+// import { useRouteMatch } from 'react-router-dom';
 import Heading from '../../components/Molecules/Text/Heading';
 import SignInForm from '../../components/Organisms/Forms/auth/SignInForm';
 
 export default function Signin() {
-    const {path} = useRouteMatch();
+  //   const { path } = useRouteMatch();
 
-    return (
-        <React.Fragment>
-            <div className="container-fluid bg-purple">
-                <div className="page-container">
-                    <div className="content bg-white col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-                        <div className="form-container">
-                            <div className="header">
-                                <div className="image-container">
-                                    <img src={'/assets/images/sass-logo.png'} width={'100'} alt="Logo"/>
-                                </div>
-                                <div className="form-title mt-4">
-                                    <Heading text={'Prisijungimas'} color={'dark-blue'} weight={'bold'} />
-                                </div>
-                            </div>
-                            <div className="body mt-4">
-                                    <SignInForm/>
-                            </div>
-                            <div className="footer">
-                                <p className="text-center meta-txt">Neturite prisijungimo? <span className="meta-colored">Sukurti</span> </p>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <React.Fragment>
+      <div className="container-fluid bg-purple">
+        <div className="page-container">
+          <div className="content bg-white col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+            <div className="form-container">
+              <div className="header">
+                <div className="image-container">
+                  <img src={'/assets/images/sass-logo.png'} width={'100'} alt="Logo" />
                 </div>
+                <div className="form-title mt-4">
+                  <Heading text={'Prisijungimas'} color={'dark-blue'} weight={'bold'} />
+                </div>
+              </div>
+              <div className="body mt-4">
+                <SignInForm />
+              </div>
+              <div className="footer">
+                <p className="text-center meta-txt">
+                  Neturite prisijungimo? <span className="meta-colored">Sukurti</span>{' '}
+                </p>
+              </div>
             </div>
-        </React.Fragment>
-    )
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
