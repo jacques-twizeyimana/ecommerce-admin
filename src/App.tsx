2400c530411f2d0000231f7aeb3beb15c427f2d8
divinirakiza <irakizadivin@gmail.com>
2022-05-05 22:44:51 +0200

    feat: employee details page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 
+import MainLayout from './layouts/MainLayout';
 import ShowCase from './pages/__test/Showcase';
 import Signin from './pages/auth/Signin';
 import Employees from './pages/employees';
+import EmployeeDetails from './pages/employees/EmployeeDetails';
 
 const App = () => {
   return (
@@ -13,7 +15,13 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Signin />} />
-        <Route path="/employees" element={<Employees />} />
+
+        {/* dashboard */}
+        <Route path="dashboard" element={<MainLayout />}>
+          <Route path="employees" element={<Employees />} />
+          <Route path="employees/:id" element={<EmployeeDetails />} />
+        </Route>
+
         <Route path="/usage" element={<ShowCase />} />
       </Routes>
     </Router>
