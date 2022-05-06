fffe5d1d6ddc21cc182dcc7e13d0159bc36408e9
Divin Irakiza <irakizadivin@gmail.com>
2022-05-06 23:13:29 +0200

    feat: done with employees integration

--- src/components/Organisms/employees/EmployeeInfo.tsx
+++ src/components/Organisms/employees/EmployeeInfo.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { EmployeeDto } from '../../../types/services/employees.types';
 
-import {
-  basicInfo,
-  clothingSize,
-  contactInfo,
-  drivingLicence,
-  otherInfo,
-  residenceInfo,
-  workingCondition,
-} from '../../../utils/static/employee';
+
 import Button from '../../Molecules/Button/Button';
 import Details from '../../Molecules/custom/Details';
 import PopupModal from '../Modals/PopupModal';
@@ -19,140 +11,149 @@ export default function EmployeeInfo(props: {employee: EmployeeDto}) {
   const [popupModalShow, setPopupModalShow] = React.useState(false);
   const [rightModalShow, setRightModalShow] = React.useState(false);
 
+
+
+  let basicInfo = {}, workingCondition = {}, residenceInfo = {}, drivingLicense={}, clothingSize={}, otherInfo={}, contactInfo={}; 
+
   console.log(props.employee);
 
-  const basicInfo = {
-    Darbo: props.employee?.employeeRole.name,
-    'Grafiko Nr.': props.employee?.address,
-    Pareigos: props.employee.employeeRole?.name,
-    'Sutarties Nr.': props.employee.contractNumber,
-    'Įdarbinimo pradžia': props.employee.startDate.toLocaleDateString(),
-    'Sodros Nr.': props.employee.seqNumber,
-    'Pirmoji darbo diena': '',
-    Šalis: props.employee.country,
-    Etatas: props.employee.city,
-    'Asmens kodas': '0000000000',
-    'Darbo savaitės trukmė': '6 dienos',
-    Darbuotojas: props.employee.firstName + ' ' + props.employee.lastName,
-    Statusas: (props.employee.isActive) ? 'Active': 'InActive',
-  };
+  if (props.employee) {
+      basicInfo = {
+        Darbo: props.employee?.employeeRole.name,
+        'Grafiko Nr.': props.employee?.address,
+        Pareigos: props.employee.employeeRole?.name,
+        'Sutarties Nr.': props.employee.contractNumber,
+        'Įdarbinimo pradžia': props.employee.startDate,
+        'Sodros Nr.': props.employee.seqNumber,
+        'Pirmoji darbo diena': '',
+        Šalis: props.employee.country,
+        Etatas: props.employee.city,
+        'Asmens kodas': '0000000000',
+        'Darbo savaitės trukmė': '6 dienos',
+        Darbuotojas: props.employee.firstName + ' ' + props.employee.lastName,
+        Statusas: (props.employee.isActive) ? 'Active': 'InActive',
+      };
 
-  const workingCondition = {
-      Atlyginimas: props.employee.salary,
-      Gyvenamoji: props.employee.address,
-      Telefonas: props.employee.phone,
-      //   'Gyvenamoji vieta': 'nėra duomenų',
-      'El. paštas': props.employee.email,
-      Adresas: props.employee.address,
-  };
+      workingCondition = {
+          Atlyginimas: props.employee.salary,
+          Gyvenamoji: props.employee.address,
+          Telefonas: props.employee.phone,
+          //   'Gyvenamoji vieta': 'nėra duomenų',
+          'El. paštas': props.employee.email,
+          Adresas: props.employee.address,
+      };
 
-  const residenceInfo = {
-    // Gyvenamoji vieta
-      'Gyvenamoji vieta': props.employee.address,
-      Adresas: props.employee.address,
-  };
-  
-  const drivingLicence = {
-    // Vairuotojo pažymėjimas
-    Numeris: props.employee.drivingLicense.name,
-    Kategorija: props.employee.drivingLicense.name,
-  };
-  
-  const clothingSize = {
-    Batai: props.employee.clothings[1].type.name,
-    Kelnės: props.employee.clothings[0].type.name,
-    Švarkas: props.employee.clothings[0].type.name,
-  };
-  
+      residenceInfo = {
+        // Gyvenamoji vieta
+          'Gyvenamoji vieta': props.employee.address,
+          Adresas: props.employee.address,
+      };
+      
+      drivingLicense = {
+        // Vairuotojo pažymėjimas
+        Numeris: props.employee.drivingLicense.name,
+        Kategorija: props.employee.drivingLicense.name,
+      };
+      
+      clothingSize = {
+        Batai: props.employee.clothings[1].type.name,
+        Kelnės: props.employee.clothings[0].type.name,
+        Švarkas: props.employee.clothings[0].type.name,
+      };
+      
+      otherInfo = {
+        Informacija: props.employee.otherInfo,
+      };
 
-  const otherInfo = {
-    Informacija: props.employee.otherInfo,
-  };
-
-
-  const contactInfo = {
-    // Kontaktiniai duomenys
-    'Grafiko Nr.': props.employee.contractNumber,
-    'Sutarties Nr.': props.employee.contractNumber,
-    'Sodros Nr.': props.employee.seqNumber,
-    Šalis: props.employee.country,
-    'Asmens kodas': props.employee.personalIdentificationNumber,
-    Darbuotojas: props.employee.firstName + ' ' + props.employee.lastName,
-    Statusas: (props.employee.isActive) ? 'Active': 'InActive',
-  };
+      contactInfo = {
+        // Kontaktiniai duomenys
+        'Grafiko Nr.': props.employee.contractNumber,
+        'Sutarties Nr.': props.employee.contractNumber,
+        'Sodros Nr.': props.employee.seqNumber,
+        Šalis: props.employee.country,
+        'Asmens kodas': props.employee.personalIdentificationNumber,
+        Darbuotojas: props.employee.firstName + ' ' + props.employee.lastName,
+        Statusas: (props.employee.isActive) ? 'Active': 'InActive',
+      };
+}
   
 
   return (
-    <div className="py-4 px-5 bg-white">
-      <div className="p-2 border d-inline-block">
-        <div className=" w-20 h-20 border rounded-circle text-center text-sm">
-          {/* Photo placeholder */}
-          <img src={props.employee.profileUrl!} width={85} style={{borderRadius: '50%', marginLeft: '-3px', marginTop: '1px'}} alt='Profile url'/>
+    <React.Fragment>
+      {props.employee ? 
+        <div className="py-4 px-5 bg-white">
+        <div className="p-2 border d-inline-block">
+          <div className=" w-20 h-20 border rounded-circle text-center text-sm">
+            {/* Photo placeholder */}
+            <img src={props.employee?.profileUrl!} width={85} style={{borderRadius: '50%', marginLeft: '-3px', marginTop: '1px'}} alt='Profile url'/>
+          </div>
         </div>
-      </div>
-      <div className="action py-3 row">
-        {/* <button className="d-inline-block w-auto py-3 text-xs text-lowercase rounded">
-          Atnaujinti duomenis
-        </button> */}
-        <div className="col-3 mr-3">
-          <Button
-            onClick={() => setRightModalShow(true)}
-            className="text-capitalize b-radius">
+        <div className="action py-3 row">
+          {/* <button className="d-inline-block w-auto py-3 text-xs text-lowercase rounded">
             Atnaujinti duomenis
-          </Button>
-        </div>
-        <div className="col-3 ml-3">
-          <Button
-            className="text-capitalize b-radius light"
-            onClick={() => setPopupModalShow(true)}>
+          </button> */}
+          <div className="col-3 mr-3">
+            <Button
+              onClick={() => setRightModalShow(true)}
+              className="text-capitalize b-radius">
+              Atnaujinti duomenis
+            </Button>
+          </div>
+          <div className="col-3 ml-3">
+            <Button
+              className="text-capitalize b-radius light"
+              onClick={() => setPopupModalShow(true)}>
+              Archyvuoti darbuotoją
+            </Button>
+          </div>
+          {/* <button className="mx-2 d-inline-block w-auto py-3 text-sm text-lowercase rounded">
             Archyvuoti darbuotoją
-          </Button>
+          </button> */}
         </div>
-        {/* <button className="mx-2 d-inline-block w-auto py-3 text-sm text-lowercase rounded">
-          Archyvuoti darbuotoją
-        </button> */}
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <Details title="Duomenys" data={basicInfo} />
+          </div>
+          <div className="col-12 col-md-6">
+            <Details title="Kontaktiniai duomenys" data={contactInfo} />
+          </div>
+  
+          <div className="col-12 col-md-6">
+            <Details title="Kontaktiniai duomenys" data={workingCondition} />
+          </div>
+  
+          <div className="col-12 col-md-6">
+            <Details title="Gyvenamoji vieta" data={residenceInfo} />
+          </div>
+  
+          <div className="col-12 col-md-6">
+            <Details title="Vairuotojo pažymėjimas" data={drivingLicense} />
+          </div>
+  
+          <div className="col-12 col-md-6">
+            <Details title="Avalynės ir drabužių dydžiai" data={clothingSize} />
+          </div>
+          <div className="col-12 col-md-6">
+            <Details title="Kita info" data={otherInfo} />
+          </div>
+        </div>
+        <PopupModal
+          show={popupModalShow}
+          onHide={() => {
+            setPopupModalShow(false);
+          }}
+          setShow={setPopupModalShow}
+        />
+  
+        <SideModal
+          show={rightModalShow}
+          className={'side-modal'}
+          setShow={setRightModalShow}
+          onHide={() => setRightModalShow(false)}
+        />
       </div>
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <Details title="Duomenys" data={basicInfo} />
-        </div>
-        <div className="col-12 col-md-6">
-          <Details title="Kontaktiniai duomenys" data={contactInfo} />
-        </div>
-
-        <div className="col-12 col-md-6">
-          <Details title="Kontaktiniai duomenys" data={workingCondition} />
-        </div>
-
-        <div className="col-12 col-md-6">
-          <Details title="Gyvenamoji vieta" data={residenceInfo} />
-        </div>
-
-        <div className="col-12 col-md-6">
-          <Details title="Vairuotojo pažymėjimas" data={drivingLicence} />
-        </div>
-
-        <div className="col-12 col-md-6">
-          <Details title="Avalynės ir drabužių dydžiai" data={clothingSize} />
-        </div>
-        <div className="col-12 col-md-6">
-          <Details title="Kita info" data={otherInfo} />
-        </div>
-      </div>
-      <PopupModal
-        show={popupModalShow}
-        onHide={() => {
-          setPopupModalShow(false);
-        }}
-        setShow={setPopupModalShow}
-      />
-
-      <SideModal
-        show={rightModalShow}
-        className={'side-modal'}
-        setShow={setRightModalShow}
-        onHide={() => setRightModalShow(false)}
-      />
-    </div>
+      : null}
+  
+    </React.Fragment>
   );
 }
