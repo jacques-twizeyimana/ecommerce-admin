4c8cf3374445dafb37c7705284a973dbd5412714
divinirakiza <irakizadivin@gmail.com>
2022-05-07 20:36:30 +0200

    feat: logout

--- src/components/Molecules/Avatar/Avatar.tsx
+++ src/components/Molecules/Avatar/Avatar.tsx
@@ -1,22 +1,45 @@
 import '../../../styles/components/Molecules/Avatar/Avatar.scss';
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import useAuthenticator from '../../../hooks/useAuthenticator';
+import { queryClient } from '../../../plugins/react-query';
+import Dropdown from '../../Atoms/custom/Dropdown';
 
 export default function Avatar() {
   const { user } = useAuthenticator();
-  console.log('User', user);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('jwt_info');
+    navigate('/login');
+    queryClient.clear();
+  };
+
   return (
     <div className="avatar d-flex">
-      <span className="circle"></span>
-      <p
-        className="mb-0"
-        data-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="false">
-        {user?.username}
-      </p>
+      <span className="circle" />
+      <Dropdown
+        header={
+          <p
+            className="mb-0"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false">
+            {user?.username}
+          </p>
+        }>
+        <div className="px-3 py-2 shadow rounded bg-white">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a
+            href="#"
+            onClick={handleLogout}
+            className="text-sm text-black text-decoration-none">
+            Logout
+          </a>
+        </div>
+      </Dropdown>
     </div>
   );
 }
